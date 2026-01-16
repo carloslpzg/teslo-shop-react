d7965fa081e1da63c12786ecf4b261c1fb1c9ca0
Carlos Lopez <carlos_lpzg@outlook.com>
2026-01-15 22:00:52 -0600

    Initial commit

---
+++ src/components/custom/CustomPagination.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react"
+import { useSearchParams } from "react-router";
+
+interface Props {
+    totalPages: number;
+}
+
+export const CustomPagination = ({ totalPages }: Props) => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const queryPage = searchParams.get('page') ?? '1';
+    const page = isNaN(+queryPage) ? 1 : +queryPage;
+
+    const handlePageChange = (page: number) => {
+        if (page < 1 || page > totalPages) return;
+
+        searchParams.set('page', page.toString());
+
+        setSearchParams(searchParams);
+    }
+
+    return (
+        <div className="flex items-center justify-center space-x-2">
+            <Button
+                onClick={() => handlePageChange(page - 1)}
+                disabled={page === 1}
+                variant="outline" size="sm">
+                <ChevronLeft className="h-4 w-4" />
+                Anteriores
+            </Button>
+            {
+                Array.from({ length: totalPages }).map((_, index) => (
+                    <Button
+                        onClick={() => handlePageChange(index + 1)}
+                        key={index}
+                        variant={
+                            page === index + 1 ? 'default' : 'outline'
+                        } size="sm">
+                        {index + 1}
+                    </Button>
+                ))
+            }
+
+            {/* <Button variant="outline" size="sm">
+                2
+            </Button> */}
+            {/* <Button variant="ghost" size="sm" disabled>
+                <MoreHorizontal className="h-4 w-4" />
+            </Button> */}
+
+            <Button
+                onClick={() => handlePageChange(page + 1)}
+                disabled={page === totalPages}
+                variant="outline" size="sm">
+                Siguientes
+                <ChevronRight className="h-4 w-4" />
+            </Button>
+        </div>
+    )
+}
